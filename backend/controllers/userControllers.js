7c53866d2d36dc7b5bd7f94acb128598f3d7342e
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-09-21 21:49:20 +0530

    made changes

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -7,4 +7,24 @@ const createToken = (id) => {
 
 const loginUser = async (req,res) => {
     const {name,password} = req.body;
+    try {
+        const user = await User.login(name,password);
+        const token = createTOken(user._id);
+        return res.status(200).json({...user,token});
+    } catch (error) {
+        return res.status(404).json({error:error.message});
+    }
+};
+
+const signupUser = async (req,res) => {
+    const {name,email,password} = req.body;
+    try {
+        const user = await User.signup(name,email,password);
+        const token = createToken(user._id);
+        return res.status(200).json({...user,token});
+    } catch (error) {
+        return res.status(404).json({error:error.message});
+    }
 }
+
+module.exports = {loginUser,signupUser};
