31b3f98b92e5d3b88ee0fe5c8eed356ecd6eff10
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-09-22 23:18:46 +0530

    made changes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,3 +4,7 @@ const multer  = require('multer');
 const {v2:cloudinary} = require('cloudinary');
 const fs      = require('fs');
 const path    = require('path');
+
+const {
+    signupUser, loginUser
+}             = require('../controllers/userControllers');
