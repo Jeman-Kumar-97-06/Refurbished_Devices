5973125de5984e9bf686762621beed2ea20d11ad
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-09-28 22:01:02 +0530

    made changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import GreenGadgetsAd from '../components/Landing'
-import HomePage from '../components/Main'
-import ProductDetail from '../components/ProductDets'
-import CartPage from '../components/CartPage'
-import AuthPage from '../components/AuthPage'
+import GreenGadgetsAd from './components/Landing'
+import HomePage from './components/Main'
+import ProductDetail from './components/ProductDets'
+import CartPage from './components/CartPage'
+import AuthPage from './components/AuthPage'
 function App() {
   
   return (
